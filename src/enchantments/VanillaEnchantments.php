936129b6cec3e29ef4cbcb275a3dbbe3ec204648
Vasync <tbao12901@gmail.com>
2024-08-24 14:36:14 +0700

    Update VanillaEnchantments.php

--- src/enchantments/VanillaEnchantments.php
+++ src/enchantments/VanillaEnchantments.php
@@ -58,7 +58,7 @@ use pocketmine\utils\RegistryTrait;
 final class VanillaEnchantments{
 	use RegistryTrait;
 
-	protected static function setup() : void{
+	public static function setup() : void{
 		self::register("PROTECTION", new ProtectionEnchantment(
 			"Protection",
 			Rarity::COMMON,
