e7d30e6d6134553512e48b90dd836a6baf960d38
Vasync <tbao12901@gmail.com>
2024-08-25 23:53:31 +0700

    Create CustomiesSound.php

---
+++ src/sound/CustomiesSound.php
@@ -0,0 +1,17 @@
+<?php
+declare(strict_types=1);
+
+namespace customiesdevs\customies\particle;
+
+use pocketmine\world\sound\Sound;
+use pocketmine\math\Vector3;
+use pocketmine\network\mcpe\protocol\PlaySoundPacket;
+
+class CustomiesSound implements Sound {
+        
+        public function __construct(private readonly string $name, private float $volume = 1) {}
+        
+        public function encode(Vector3 $pos): array { 
+                return [PlaySoundPacket::create($this->name, $pos->getX(), $pos->getY(), $pos->getZ(), $this->volume, 0)];
+        }
+}                                                                       
